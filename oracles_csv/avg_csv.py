5a3ebca2ace4dc2471ddae616615ba44e99d5a3d
andytuantruong <andytuantruong@gmail.com>
2024-03-07 15:46:51 -0800

    sorting avg kills by descending order

--- oracles_csv/avg_csv.py
+++ oracles_csv/avg_csv.py
@@ -7,4 +7,6 @@ extracted_data = data[['Player', 'GP', 'K']].copy()
 #accounting for two maps played
 extracted_data['Avg'] = (extracted_data['K'] / extracted_data['GP']) * 2
 
-print(extracted_data)
+extracted_data_sorted = extracted_data.sort_values(by='Avg', ascending=False)
+
+print(extracted_data_sorted)
