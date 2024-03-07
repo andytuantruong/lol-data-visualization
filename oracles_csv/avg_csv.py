2bf151f5bc6bc8a43e92f58aee88889a48af26cd
andytuantruong <andytuantruong@gmail.com>
2024-03-07 13:57:21 -0800

    multiplying the Avg by 2 to account for the first two maps being played

--- oracles_csv/avg_csv.py
+++ oracles_csv/avg_csv.py
@@ -3,6 +3,8 @@ import pandas as pd
 data = pd.read_csv('player_stats.csv')
 
 extracted_data = data[['Player', 'GP', 'K']]
-extracted_data['Avg'] = extracted_data['K'] / extracted_data['GP']
+
+#accounting for two maps played
+extracted_data['Avg'] = (extracted_data['K'] / extracted_data['GP']) * 2
 
 print(extracted_data)
