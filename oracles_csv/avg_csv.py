7b836a69c8a57b6820270073abaafb234716f6e5
andytuantruong <andytuantruong@gmail.com>
2024-03-07 15:41:42 -0800

    updating setting with copy warning
    SettingWithCopyWarning:
    A value is trying to be set on a copy of a slice from a DataFrame.
    Using a copy to offset this.

--- oracles_csv/avg_csv.py
+++ oracles_csv/avg_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 data = pd.read_csv('player_stats.csv')
 
-extracted_data = data[['Player', 'GP', 'K']]
+extracted_data = data[['Player', 'GP', 'K']].copy()
 
 #accounting for two maps played
 extracted_data['Avg'] = (extracted_data['K'] / extracted_data['GP']) * 2
