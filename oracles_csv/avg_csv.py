0eed5f8788b764419e10118da6fa2726fa0f9022
andytuantruong <andytuantruong@gmail.com>
2024-03-09 20:41:28 -0800

    sorting between match kills and set prop line to find biggest differences

--- oracles_csv/avg_csv.py
+++ oracles_csv/avg_csv.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-data = pd.read_csv('player_stats.csv')
+output_data = pd.read_csv('output.csv')
+data = pd.read_csv('lpl_march9.csv')
 
 extracted_data = data[['Player', 'GP', 'K']].copy()
 
@@ -8,5 +9,8 @@ extracted_data = data[['Player', 'GP', 'K']].copy()
 extracted_data['Avg'] = (extracted_data['K'] / extracted_data['GP']) * 2
 
 extracted_data_sorted = extracted_data.sort_values(by='Avg', ascending=False)
+combined_data = pd.merge(extracted_data_sorted, output_data[['Name', 'prop']], how='left', left_on='Player', right_on='Name')
+combined_data['Difference'] = combined_data['Avg'] - combined_data['prop']
+combined_data = combined_data.sort_values(by='Difference', ascending=False)
 
-print(extracted_data_sorted)
+print(combined_data)
