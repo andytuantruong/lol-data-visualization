9e238bc40864e76fd5bc3bbb73b1dd8f353dacb5
andytuantruong <andytuantruong@gmail.com>
2024-03-09 22:38:46 -0800

    fixed bug where player key name is case sensitive and wouldn't read in to comparison

--- oracles_csv/avg_csv.py
+++ oracles_csv/avg_csv.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-output_data = pd.read_csv('output.csv')
-data = pd.read_csv('lpl_march9.csv')
+output_data = pd.read_csv('~/OneDrive/Documents/lol data visualization/parser/output.csv')
+data = pd.read_csv('lec_march10.csv')
 
 extracted_data = data[['Player', 'GP', 'K']].copy()
 
 #accounting for two maps played
-extracted_data['Avg'] = (extracted_data['K'] / extracted_data['GP']) * 2
+extracted_data['Avg'] = (extracted_data['K'] / extracted_data['GP'])
 
-extracted_data_sorted = extracted_data.sort_values(by='Avg', ascending=False)
-combined_data = pd.merge(extracted_data_sorted, output_data[['Name', 'prop']], how='left', left_on='Player', right_on='Name')
+combined_data = pd.merge(extracted_data, output_data[['Name', 'prop']], how='left', 
+                         left_on=extracted_data['Player'].str.lower(), 
+                         right_on=output_data['Name'].str.lower())
+
 combined_data['Difference'] = combined_data['Avg'] - combined_data['prop']
 combined_data = combined_data.sort_values(by='Difference', ascending=False)
+combined_data.drop(columns=['key_0'], inplace=True)
 
 print(combined_data)
